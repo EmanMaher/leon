5362ec1909e968ce480dea004803400eccf0e8cc
Eman Maher <emanmaher707@gmail.com>
2021-08-14 20:28:10 +0200

    upload

---
+++ js/master.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	'use strict';
+
+/*
+ Usung jquery functions
+*/
+//adjust the height of header overlay
+
+//    // $('.backg').resizable();
+// var screenWidth  = $(window).width(),
+//     screenHeight = $(window).height();
+    
+//   $(' .landing').height((screenHeight-100));
+
+//  $(window).resize(function(){
+//    $(.'landing ').height((screenHeight-100));
+//  });
+
+
+
+
+});
